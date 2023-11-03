cdd00802441e27dd6ecfb00cd0d7473e64e9c63b
aryanmewar <149574189+aryanmewar@users.noreply.github.com>
2023-11-03 06:52:59 -0400

    Create get days by number.c

---
+++ get days by number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main ()
+{
+ int a;
+ printf("enter a number to take the date :");
+ scanf("%d", &a);
+if (a==1)
+{printf("the day is monday");}
+else if (a==2)
+{printf("the day is tuesday");}
+else if (a==3)
+{printf("the day is wednesday");}
+else
+{printf("you enter wrong number");}
+}
